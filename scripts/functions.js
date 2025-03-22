988752b84bb3a4b4c9331ba1612de9334daab362
Antonio Iliyanov <antildev@proton.me>
2025-03-22 19:59:02 +0100

    feat(input): add clipboard paste functionality feat(storage): implement URL saving to localStorage
    fix(buttons): improve URL button styling
    fix(layout): adjust buttons arrangement
    fix(list-items): remove hover effect from list items

--- scripts/functions.js
+++ scripts/functions.js
@@ -9,9 +9,56 @@ function extractUrls(text) {
   }).flat()
 }
 
-function openRandomUrl(urlList) {
-  let randomPos = Math.floor(Math.random() * urlList.length)
+function addUrls(urls, urlList, openUrlButton, message) {
+  if (!urls || urls.length === 0) {
+    urlList.textContent = message
+    return
+  }
+
+  urlList.textContent = ""
+
+  urls.forEach(element => {
+    const div = document.createElement('div');
+    div.className = 'list-item'
+    
+    const url = document.createElement('a');
+    url.href = element
+    url.target = '_blank'
+    url.textContent = element
+    
+    const removeBtn = document.createElement('i');
+    removeBtn.id = 'remove-item'
+    removeBtn.className = 'bx bx-x'
+    
+    div.appendChild(url)
+    div.appendChild(removeBtn)
+    urlList.appendChild(div)
+    
+    removeBtn.addEventListener('click', function () {
+      const listItem = this.parentElement
+      urls.splice(urls.indexOf(this.parentElement.querySelector('a').href), 1)
+      listItem.remove()
+
+      if (urlList.children.length === 0) {
+        openUrlButton.disabled = true
+        urlList.textContent = message
+        urlList.style.display = 'flex'
+      }
+    })
+  })
+
+  urlList.style.display = 'block'
+  openUrlButton.disabled = false
+}
+
+function openRandomUrl(urls) {
+  let randomPos = Math.floor(Math.random() * urls.length)
   let urlsList = document.querySelectorAll('#url-list .list-item a')
   
   window.open(urlsList[randomPos].href, '_blank').focus()
+}
+
+async function pasteFromClipboard(element) {
+  const text = await navigator.clipboard.readText()
+  element.textContent = text
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,56 @@
 document.addEventListener("DOMContentLoaded", function () {
   console.log("link start")
 
-  let editableDiv = document.querySelector('#editable-div')
-  let openUrl = document.querySelector('#open-url')
-  let urlList = document.querySelector('#url-list')
-  let urlCount = document.querySelector('#url-count')
+  const editableDiv = document.querySelector('#editable-div')
+  const openUrl = document.querySelector('#open-url')
+  const urlList = document.querySelector('#url-list')
+  const urlCount = document.querySelector('#url-count')
+  const emptyUrlListMessage = "No URLs found."
 
-  let urls = []
+  let urls = JSON.parse(localStorage.getItem('urls'))
+
+  addUrls(urls, urlList, openUrl, emptyUrlListMessage)
+  urlCount.textContent = urls ? `Total URLs: ${urls.length}` : "Total URLs: 0"
+
+  document.addEventListener('click', function (e) {
+    if (editableDiv.textContent.trim() === '' && e.target.id != 'editable-div') {
+      editableDiv.textContent = "Paste here your URLs."
+      editableDiv.blur()
+    }
+  })
 
   editableDiv.addEventListener('click', function () {
     if (editableDiv.textContent.trim() === 'Paste here your URLs.') editableDiv.textContent = ""
   })
 
-  editableDiv.addEventListener('blur', function () {
-    if (editableDiv.textContent.trim() === '') editableDiv.textContent = 'Paste here your URLs.'
+  document.querySelector('#paste-urls').addEventListener('click', async function () {
+    try {
+      await pasteFromClipboard(editableDiv)
+    } catch (error) {
+      console.error('Failed to paste: ', error)
+    }
   })
 
   document.querySelector('#add-urls').addEventListener('click', function () {
     urls = extractUrls(editableDiv.textContent)
-    
-    if (urls.length === 0) {
-      urlList.textContent = "Oops, no valid URLs found."
-      return
-    }
-    
-    urlList.textContent = ""
-    
-    urls.forEach(element => {
-      const div = document.createElement('div');
-      div.className = 'list-item'
-      
-      const url = document.createElement('a');
-      url.href = element
-      url.target = '_blank'
-      url.textContent = element
-      
-      const removeBtn = document.createElement('i');
-      removeBtn.id = 'remove-item'
-      removeBtn.className = 'bx bx-x'
-      
-      div.appendChild(url)
-      div.appendChild(removeBtn)
-      urlList.appendChild(div)
-      
-      removeBtn.addEventListener('click', function () {
-        const listItem = this.parentElement
-        urls.splice(urls.indexOf(this.parentElement.querySelector('a').href), 1)
-        listItem.remove()
-
-        if (urlList.children.length === 0) {
-          openUrl.disabled = true
-          urlList.textContent = "No URLs found!"
-          urlList.style.display = 'flex'
-        }
-      })
-    })
-
-    urlList.style.display = 'block'
-    urlCount.textContent = `Total URLs: ${urls.length}`
-    openUrl.disabled = false
+    addUrls(urls, urlList, openUrl, emptyUrlListMessage)
   })
 
   document.querySelector('#clear-urls').addEventListener('click', function () {
+    urls = ""
     openUrl.disabled = true
     urlList.textContent = "URL list cleared!"
     urlList.style.display = 'flex'
     urlCount.textContent = "Total URLs: 0"
+  })
+
+  document.querySelector('#save-urls').addEventListener('click', function () {
+    if (!urls || urls.length === 0) {
+      window.alert("No URLs found. Add some URLs first")
+      return
+    }
+
+    localStorage.setItem('urls', JSON.stringify(urls))
   })
 
   openUrl.addEventListener('click', function () {
